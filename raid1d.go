b126fc0078e7af4ac49c4d2d92d931b6839e5e03
Azat Seidakhmet <seidakhmetazat@gmail.com>
2019-10-19 14:40:22 +0600

    raid c, d, e

---
+++ raid1d.go
@@ -0,0 +1,22 @@
+package student
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func Raid1d(a int, b int) {
+	for i := 0; i < b; i++ {
+		for j := 0; j < a; j++ {
+			if (i == 0 && j == 0) || (i == b-1 && j == 0) {
+				z01.PrintRune('A')
+			} else if (i == 0 && j == a-1) || (j == a-1 && i == b-1) {
+				z01.PrintRune('C')
+			} else if i == 0 || i == b-1 || j == 0 || j == a-1 {
+				z01.PrintRune('B')
+			} else {
+				z01.PrintRune(' ')
+			}
+		}
+		z01.PrintRune('\n')
+	}
+}
