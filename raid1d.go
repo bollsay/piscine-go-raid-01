27658c10543138866ae31faf0c9a4bc71c68ab05
Azat Seidakhmet <seidakhmetazat@gmail.com>
2019-10-19 14:48:35 +0600

    test

--- raid1d.go
+++ raid1d.go
@@ -6,15 +6,17 @@ import (
 
 func Raid1d(a int, b int) {
 	for i := 0; i < b; i++ {
-		for j := 0; j < a; j++ {
-			if (i == 0 && j == 0) || (i == b-1 && j == 0) {
-				z01.PrintRune('A')
-			} else if (i == 0 && j == a-1) || (j == a-1 && i == b-1) {
-				z01.PrintRune('C')
-			} else if i == 0 || i == b-1 || j == 0 || j == a-1 {
-				z01.PrintRune('B')
-			} else {
-				z01.PrintRune(' ')
+		if a > 0 {
+			for j := 0; j < a; j++ {
+				if (i == 0 && j == 0) || (i == b-1 && j == 0) {
+					z01.PrintRune('A')
+				} else if (i == 0 && j == a-1) || (j == a-1 && i == b-1) {
+					z01.PrintRune('C')
+				} else if i == 0 || i == b-1 || j == 0 || j == a-1 {
+					z01.PrintRune('B')
+				} else {
+					z01.PrintRune(' ')
+				}
 			}
 		}
 		z01.PrintRune('\n')
